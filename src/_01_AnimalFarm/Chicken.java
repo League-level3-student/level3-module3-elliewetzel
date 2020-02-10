1458089df2822f681405005b10e4fb15c30ff2d1
League Student <league@iMac-12.attlocal.net>
2020-02-09 17:24:45 -0800

    Animal, Chicken, Cow, Farm, Pig, Sheep, Atlanta, Boston, City, Los Angeles, San Diego

---
+++ src/_01_AnimalFarm/Chicken.java
@@ -0,0 +1,18 @@
+package _01_AnimalFarm;
+
+public class Chicken extends Animal {
+
+	@Override
+	public void makeNoise() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void sleep() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	
+}
